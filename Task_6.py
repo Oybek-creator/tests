c896a82dcc39072b9e535bd9c1258bdc1dcfbade
omutalov95 <Aa19952804>
2020-12-03 21:07:45 -0500

    Task_6_2 completed

--- Task_6.py
+++ Task_6.py
@@ -1,3 +1,8 @@
 from random import randint
 n = 3
 matrix = [[randint(1, 9) for j in range(n)]for i in range(n)]
+
+"""Найти максимальный элемент матрицы."""
+
+print(matrix)
+print(max(map(max, matrix)))
